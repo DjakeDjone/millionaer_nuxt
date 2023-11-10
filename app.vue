7ca1c8364be694659e306a91cdc88e60f53e4953
DjakeDjones <benjamin.friedl@htlstp.at>
2023-11-10 16:53:16 +0100

    works

--- app.vue
+++ app.vue
@@ -13,10 +13,50 @@ const questions: Question[] = [
     correctAnswer: 2,
   },
   {
-    question: 'Was ist die Hauptstadt von Spanien?',
-    answers: ['London', 'Rom', 'Paris', 'Madrid'],
+    question: 'Wer entdeckte im Jahr 1492 Amerika?',
+    answers: ['Christoph Columbus', 'Napoleon Bonaparte', 'Magellan', 'Elon Musk'],
+    correctAnswer: 0,
+  },
+  {
+    question: 'Welche Antwortmöglichkeit ist kein Kontinent?',
+    answers: ['Europa', 'Asien', 'Arktis', 'Australien'],
+    correctAnswer: 2,
+  },
+  {
+    question: 'Wann begann der 2. Weltkrieg?',
+    answers: ['1995', '1845', '1914', '1939'],
     correctAnswer: 3,
   },
+  {
+    question: 'Wie heißt der höchste Berg Österreichs?',
+    answers: ['Schneeberg', 'Großglockner', 'Mount Everest', 'Mont Blanc'],
+    correctAnswer: 1,
+  },
+  {
+    question: 'Wer übersetzte in der Wartburg die Bibel ins Deutsche?',
+    answers: ['Martin Luther King', 'Johannes Kepler', 'Johann Wolfgang von Goethe', 'Martin Luther'],
+    correctAnswer: 3,
+  },
+  {
+    question: 'Welches Nachbarland ist nicht teil der EU?',
+    answers: ['Schweiz', 'Ungarn', 'Italien', 'Tschechische Rupublik'],
+    correctAnswer: 0,
+  },
+  {
+    question: 'Welche Adelsfamilie regierte fast 650 Jahre Österreich?',
+    answers: ['Habsburger', 'Babenberger', 'die Gallier', 'Wittelsbacher'],
+    correctAnswer: 0,
+  },
+  {
+    question: 'Über welches Land in Afrika erstreckt sich die Sahara NICHT?',
+    answers: ['Ägypten', 'Algerien', 'Marokko', 'Südafrika'],
+    correctAnswer: 3,
+  },
+  {
+    question: 'Wer ist bekannt für den Ausspruch Österreich ist frei?',
+    answers: ['Alexander Van der Bellen', 'Kaiser Fanz Joseph', 'Karl Renner', 'Leopold Figel'],
+    correctAnswer: 2,
+  }
 ]
 
 let idx = 0;
@@ -39,13 +79,14 @@ const handleAnswer = (answer: string) => {
     // alert(`Game over! Your score is ${score}`);
   }
 }
+// watch change idx
 onMounted(() => {
   const audio = new Audio('/Start_sound.mp3');
   audio.play();
   setTimeout(() => {
     status.value = 'playing';
     audio.pause();
-  }, 2000);
+  }, 8000);
 })
 </script>
 
@@ -54,8 +95,12 @@ onMounted(() => {
     <div id="logo" :class="{ 'animate-spin': status === 'loading' }">
     </div>
     <div v-if="status == 'playing' && currentQuestion">
-      <Question :question="currentQuestion.question" :answers="currentQuestion.answers"
+      <Question :idx="idx" :question="currentQuestion.question" :answers="currentQuestion.answers" :question_length="questions.length"
         :correctAnswer="currentQuestion.correctAnswer" @answer="handleAnswer" />
+        <div id="btmslider">
+            <!-- volume slider -->
+            <input type="range" min="0" :max="questions.length" class="slider" id="myRange" v-model="idx">
+        </div>
     </div>
     <div v-if="status == 'won'">
       <Winner />
@@ -78,7 +123,42 @@ body {
   font-family: sans-serif;
 }
 
-</style><style scoped>.main {
+</style><style scoped>
+#btmslider {
+  width: 100%;
+}
+.slider {
+    -webkit-appearance: none;
+    width: 100% !important;
+    height: 10px;
+    border-radius: 5px;
+    background: #ffffff;
+    outline: none;
+    opacity: 1;
+    /* green slider icon  */
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+}
+
+.slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #4CAF50;
+    cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    border-radius: 0;
+    background: #4CAF50;
+}
+
+
+.main {
   height: 100vh;
   background-color: #000000;
   background-image: url('/Bg.jpg');
--- components/question.vue
+++ components/question.vue
@@ -4,6 +4,8 @@ const props = defineProps({
     question: String,
     answers: Array,
     correctAnswer: Number,
+    idx: Number,
+    question_length: Number
 })
 
 const emit = defineEmits(['answer'])
@@ -11,7 +13,7 @@ const handleAnswerClick = (answer: String) => {
     emit('answer', answer);
 }
 onMounted(() => {
-    const audio = new Audio('/Start_sound.mp3');
+    const audio = new Audio('/QuestionMusic.wav');
     // audio
     audio.play();
 })
@@ -21,20 +23,45 @@ onMounted(() => {
     <div class="container">
         <h2 class="question">{{ question }}</h2>
         <div class="answers">
-            <button class="answer" v-for="answer in answers" :key="answer" @click="handleAnswerClick(answer)">
-                {{ answer }}
+            <button class="answer" v-for="answer, i in answers" :key="answer" @click="handleAnswerClick(answer)" :style="'animation-delay: ' + ((answers.length-i) * 0.3) + 's'">
+                <span class="ans_number">
+                    {{ String.fromCharCode(65 + i) }}: 
+                </span>
+                <span>
+                    {{ answer }}
+                </span>
             </button>
         </div>
     </div>
 </template>
 
 <style setup>
+/* animate range slider  */
+
+.ans_number {
+    padding-right: 0.5rem;
+}
+
+#btmslider {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: #eee;
+    padding: 0.5rem;
+    font-size: 0.8rem;
+    text-align: center;
+    color: black;
+    opacity: 0.5;
+}
 .container {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     height: 100%;
+    max-width: 40rem;
+    margin: 0 auto;
     /* background: radial-gradient(#eee, #aaa); */
     /* background-image: url('/MillionärLogo.png');
     background-size: cover;
@@ -48,25 +75,41 @@ onMounted(() => {
 }
 
 .answers {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 1rem;
+    width: 100%;
 }
 .answer {
     font-size: 1.5rem;
-    margin: 0.5rem;
-    padding: 0.5rem 1rem;
+    padding: 1rem;
     border: 1px solid black;
     border-radius: 0.5rem;
     background-color: white;
-    color: black;
+    display: flex;
+    /* justify-content: space-between; */
     cursor: pointer;
+    opacity: 0;
+    animation: 0.5s ease-out 0s 1 slideInFromLeft forwards;
+}
+.answer > span {
+    color: black;
 }
 .answer:hover {
     background-color: #eee;
 }
 .answer:active {
     background-color: #ddd;
+}
+
+@keyframes slideInFromLeft {
+    0% {
+        transform: translateX(-100%);
+        opacity: 0;
+    }
+    100% {
+        transform: translateX(0);
+        opacity: 1;
+    }
 }
 </style>
